4b2f35bf69ba5d3db1c3f1c416d02bd9c4855691
jerome.vasseur <jvasseur@zatsit.fr>
2026-03-27 16:06:01 +0100

    initial commit

---
+++ src/test/java/com/example/demo/web/GreetingControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(GreetingController.class)
+class GreetingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void greetingEndpointReturnsMessage() throws Exception {
+        mockMvc.perform(get("/api/greeting"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Bonjour depuis Spring Boot!"));
+    }
+}
